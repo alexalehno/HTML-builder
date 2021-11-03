d5257cb440925e13b69923d030d2f58a80b226c8
alexalehno <alekswakeup@gmail.com>
2021-11-03 10:23:29 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathFiles = path.join(__dirname, '/files');
+const pathFilesCopy = path.join(__dirname, '/files-copy');
+
+function copyDir() {
+  fs.mkdir(pathFilesCopy, { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    }
+
+    fs.readdir(pathFiles, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      data.forEach(file => {
+        fs.copyFile(`${pathFiles}/${file}`, `${pathFilesCopy}/${file}`, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      })
+    })
+  });
+}
+
+copyDir();
