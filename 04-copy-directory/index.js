2a59477db50f0cf9307dc4a341daecac613429da
alexalehno <alekswakeup@gmail.com>
2021-11-03 11:10:38 +0300

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,24 @@ const pathFiles = path.join(__dirname, '/files');
 const pathFilesCopy = path.join(__dirname, '/files-copy');
 
 function copyDir() {
+  fs.stat(pathFilesCopy, (err, st) => {
+    if (!err) {
+      fs.readdir(pathFilesCopy, (err, dataCopy) => {
+        if (err) {
+          throw err;
+        }
+
+        dataCopy.forEach(fileCopy => {
+          fs.unlink(`${pathFilesCopy}/${fileCopy}`, (err) => {
+            if (err) {
+              throw err;
+            }
+          });
+        })
+      })
+    }
+  })
+
   fs.mkdir(pathFilesCopy, { recursive: true }, (err) => {
     if (err) {
       throw err;
